e6dccd706ef9cb74152a00deb89e79fb1a5bd0a1
Jared Farkas and Sanyu Prado <jared@>
2019-01-24 10:39:40 -0800

    Fix specs

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function cryptoSquare(words){
+  word.split('');
+
+  return
+}
+
+$(document).ready(function(){
+  $("form#transportation_survey").submit(function(event){
+      var input = $("#sentence").val();
+      var input = romanIn(input);
+    console.log(input);
+    event.preventDefault();
+    });
+  });
